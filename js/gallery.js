4fe50ce4d24370a7079bbdee03d7c8272e4180f3
bfxy <al.werehere@gmail.com>
2015-09-27 02:30:05 +0300

    hide controls on first/last photo

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,5 @@
 // Turn off stupid slideshow
+
 $(function() {
     $('.carousel').each(function(){
         $(this).carousel({
@@ -8,23 +9,56 @@ $(function() {
 });
 
 // Keyboard controls
+
 $(document).bind('keyup', function(e) {
-	if(e.which==39){
+	if (e.which==39) {
 		$('.carousel').carousel('next');
 	}   
-	else if(e.keyCode==37){
+	else if (e.keyCode==37) {
 		$('.carousel').carousel('prev');
 	}
 });
 
-// Toggle controls when hovering over body
-$(document).ready(function() {
-	$('body').on('mouseenter', function() {
-		//$('.controls').css({'display': 'none'})
-		$('.controls').fadeIn();
+// Show controls when hovering over
+
+$(function() {
+	$('.carousel').on('mouseenter', function() {
+		$('.controls').fadeIn(100);
 	});
-	$('body').on('mouseleave', function() {
-	//$('.controls').css({'display': 'none'})
-	$('.controls').fadeOut();
+	$('.carousel').on('mouseleave', function() {
+		$('.controls').fadeOut(100);
 	});
 });
+
+// Hide left/right control on first/last photo
+
+function hideFirst() {
+	if ($("#img01").parent().parent().hasClass('active')) {
+		$(".left.carousel-control").fadeOut(100);
+	} else {
+		$(".left.carousel-control").fadeIn(100);
+	}
+}
+
+function hideLast() {
+	console.log("it runs");
+	if ($("#img10").parent().parent().hasClass('active')) {
+		$(".right.carousel-control").fadeOut(100);
+	} else {
+		$(".right.carousel-control").fadeIn(100);
+	}
+}
+
+$(hideFirst());
+$(hideLast());
+
+$(function() {
+	$('#carousel').bind('slid.bs.carousel', function(e) {
+  	hideFirst();
+  	hideLast();
+  	});
+});
+
+
+
+

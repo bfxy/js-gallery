c0f8b33d8c03aec5d3747d6fdb77ae0dd43c15e9
bfxy <al.werehere@gmail.com>
2015-09-28 01:53:04 +0300

    lil js refactoring

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,23 @@
-// Turn off stupid slideshow
+// Run stuff
+$(window).load(function () {
+	stopSlideshow();
+	hideFirst();
+	hideLast();
+	showControls('.carousel', '.controls');
+	showControls('.thumbnails', '.thumb-controls');
+});
 
-$(function() {
+
+// Turn off stupid slideshow
+function stopSlideshow() {
     $('.carousel').each(function(){
         $(this).carousel({
             interval: false
         });
     });
-});
+}
 
 // Keyboard controls
-
 $(document).bind('keyup', function(e) {
 	if (e.which==39) {
 		$('.carousel').carousel('next');
@@ -20,19 +28,17 @@ $(document).bind('keyup', function(e) {
 });
 
 // Show controls when hovering over
-
-$(function() {
-	$('.carousel').on('mouseenter', function() {
-		$('.controls').fadeIn(100);
+function showControls(target, controls) {
+	$(target).on('mouseenter', function() {
+		$(controls).fadeIn(100);
 	});
-	$('.carousel').on('mouseleave', function() {
-		$('.controls').fadeOut(100);
+	$(target).on('mouseleave', function() {
+		$(controls).fadeOut(100);
 	});
-});
+}
 
 // Hide left/right control on first/last photo
-
-function hideFirst() {
+function hideFirst() { 
 	if ($("#img01").parent().parent().hasClass('active')) {
 		$(".left.carousel-control").fadeOut(100);
 	} else {
@@ -41,16 +47,12 @@ function hideFirst() {
 }
 
 function hideLast() {
-	console.log("it runs");
 	if ($("#img10").parent().parent().hasClass('active')) {
 		$(".right.carousel-control").fadeOut(100);
 	} else {
 		$(".right.carousel-control").fadeIn(100);
 	}
 }
-
-$(hideFirst());
-$(hideLast());
 
 $(function() {
 	$('#carousel').bind('slid.bs.carousel', function(e) {

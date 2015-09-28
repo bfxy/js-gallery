a0901de73969efea6c4f4a516079662aa8b18bb5
bfxy <al.werehere@gmail.com>
2015-09-28 20:29:59 +0300

    thumbnails done

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,17 @@
 // Run stuff
 $(window).load(function () {
 	stopSlideshow();
-	hideFirst();
-	hideLast();
-	showControls('.carousel', '.controls');
-	showControls('.thumbnails', '.thumb-controls');
-	showControls('.thumbnails', '.thumbs');
+	thumbHandler();
+	//hideFirst();
+	//hideLast();
+	//showControls('.carousel', '.controls');
+	//showControls('.thumbnails', '.thumb-controls');
+	//showControls('.thumbnails', '.thumbs');
 	// navigation();
 });
 
-
-// Turn off stupid slideshow
-function stopSlideshow() {
-    $('.carousel').each(function(){
-        $(this).carousel({
-            interval: false
-        });
-    });
-}
-
 // Keyboard controls
-$(document).bind('keyup', function(e) {
+$(document).bind('keyup', function (e) {
 	if (e.which==39) {
 		$('.carousel').carousel('next');
 	}   
@@ -29,12 +20,22 @@ $(document).bind('keyup', function(e) {
 	}
 });
 
+// Turn off stupid slideshow
+function stopSlideshow() {
+    $('.carousel').each(function (){
+        $(this).carousel({
+            interval: false
+        });
+    });
+}
+
+
 // Show controls when hovering over
 function showControls(target, controls) {
-	$(target).on('mouseenter', function() {
+	$(target).on('mouseenter', function () {
 		$(controls).fadeIn(100);
 	});
-	$(target).on('mouseleave', function() {
+	$(target).on('mouseleave', function () {
 		$(controls).fadeOut(100);
 	});
 }
@@ -57,7 +58,7 @@ function hideLast() {
 }
 
 $(function() { 
-	$('#carousel').bind('slid.bs.carousel', function(e) {
+	$('#carousel').bind('slid.bs.carousel', function (e) {
   	hideFirst();
   	hideLast();
   	});
@@ -71,6 +72,15 @@ $(function() {
 			$('.allImages').stop().animate({left: - ribbonPos},time);
 			}); 
 } */
+
+// Jump to photo from thumbnails
+function thumbHandler() {
+	$('.thumb-img').on('click', function () {
+		var clickedImg = $(this).attr('id');
+		$('.carousel').carousel(parseInt(clickedImg));
+	});
+}
+
 
 
 
